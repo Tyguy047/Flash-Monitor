93311e7a00a189633b02a842edabe4c64b2b2067
Tyguy047 <getintouch@tylercaselli.com>
2026-02-09 23:31:08 -0500

    Init

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Flash_Monitor;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Flash_Monitor;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
